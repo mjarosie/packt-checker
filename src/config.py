cb3794022619c17eeb924618f598cbcc0311273f
mjarosie <jarosiewicz.maciek@gmail.com>
2017-08-15 13:13:02 +0100

    Added an example of config.py file

---
+++ src/config.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+# Output settings.
+INFO = True
+
+# SMTP settings
+SMTPserver = 'smtp.server.com'
+SMTPport = 465
+sender = 'sender@mail.com'
+recipients = ['receiver@mail.com']
+USERNAME = "sender@mail.com"
+PASSWORD = "password"
+
+# External resources settings.
+url = 'https://www.packtpub.com/packt/offers/free-learning'
+
